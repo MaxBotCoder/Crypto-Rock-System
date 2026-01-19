0b53a079e05bfe4f54cedb560be9312885ecaf06
Eternal Engineer <51864059+MaxBotCoder@users.noreply.github.com>
2026-01-18 22:39:50 -0800

    Update user-contract.sol

--- user-contract.sol
+++ user-contract.sol
@@ -18,7 +18,6 @@ contract user {
 
   //recieve payment
   function sendMoney(uint256 paymentType, uint256 amount) public {
-      halvedValue = msg.value/2;
       if (paymentType == 1) {
         creator.send{value: halvedValue, gas: 100000}("");
       } else if (paymentType == 2) {
